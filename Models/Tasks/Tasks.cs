038d85799790b0f6b9b9d459a6b408fe3e384771
[Minh <[minhdangtrannhat@yahoo.com]>
2019-11-18 09:42:33 -0600

    Initialize Create Task Page

--- Models/Tasks/Tasks.cs
+++ Models/Tasks/Tasks.cs
@@ -10,15 +10,18 @@ namespace CashewWeb.Models
     public class Tasks
     {
         [BindProperty]
-        [MaxLength(50, ErrorMessage = "TaskId may not exceed 50 characters.")]
+        [MaxLength(50, ErrorMessage = "Task ID may not exceed 50 characters.")]
         public string TaskId { get; set; }
 
         [BindProperty]
-        [MaxLength(50, ErrorMessage = "TaskId may not exceed 50 characters.")]
+        [MaxLength(50, ErrorMessage = "Task Name may not exceed 50 characters.")]
+        public string TaskName { get; set; }
+
+        [BindProperty]
         public DateTime TaskDeadline { get; set; }
 
         [BindProperty]
-        [MaxLength(65335, ErrorMessage = "TaskId may not exceed 50 characters.")]
+        [MaxLength(150, ErrorMessage = "Task Description may not exceed 150 characters.")]
         public string TaskDescription { get; set; }
     }
 }
