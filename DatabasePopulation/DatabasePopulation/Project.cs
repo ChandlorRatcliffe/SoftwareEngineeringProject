a1ce29a9ebb8e638a407129c5523f18ca696ad16
chandlor.ratcliffe <chandlor.ratcliffe@gmail.com>
2019-11-18 23:17:55 -0600

    Create Project.cs
    Created the project to be used in the generation process.

---
+++ DatabasePopulation/DatabasePopulation/Project.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabasePopulation
+{
+    class Project
+    {
+        static int id = 0;
+        DateTime deadline;
+        string description;
+        public Project()
+        {
+            id = -1;
+            deadline = DateTime.Today;
+            description = "I don't know how this got here.";
+        }
+        public Project(DateTime deadline, string description)
+        {
+            id++;
+            this.deadline = deadline;
+            this.description = description;
+        }
+        public string getAddTupleQuerryString()
+        {
+            return "(" +id + "\"" + deadline.ToString("yyyy-MM-dd HH:mm:ss") + "\",\"" + description+ "\")";
+        }
+    }
+}
