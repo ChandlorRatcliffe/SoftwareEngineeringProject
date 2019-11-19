84fe55674bad1051386b110813038e3a2f46f5e9
chandlor.ratcliffe <chandlor.ratcliffe@gmail.com>
2019-11-18 23:26:14 -0600

    Created ProjectGenerator
    Created the Project Generator, and fixed the id generation for Projects.

--- DatabasePopulation/DatabasePopulation/Project.cs
+++ DatabasePopulation/DatabasePopulation/Project.cs
@@ -7,23 +7,24 @@ namespace DatabasePopulation
     class Project
     {
         static int id = 0;
+        int myId;
         DateTime deadline;
         string description;
         public Project()
         {
-            id = -1;
+            myId = -1;
             deadline = DateTime.Today;
             description = "I don't know how this got here.";
         }
         public Project(DateTime deadline, string description)
         {
-            id++;
+            myId = id++;
             this.deadline = deadline;
             this.description = description;
         }
         public string getAddTupleQuerryString()
         {
-            return "(" +id + "\"" + deadline.ToString("yyyy-MM-dd HH:mm:ss") + "\",\"" + description+ "\")";
+            return "(" +myId + "\"" + deadline.ToString("yyyy-MM-dd HH:mm:ss") + "\",\"" + description+ "\")";
         }
     }
 }
