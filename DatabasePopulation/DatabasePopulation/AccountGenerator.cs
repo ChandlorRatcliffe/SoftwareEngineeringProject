65bb182e6372cc7be67aa8af5c52474f950a46f2
chandlor.ratcliffe <chandlor.ratcliffe@gmail.com>
2019-11-22 22:07:24 -0600

    Documented AccountGenerator

--- DatabasePopulation/DatabasePopulation/AccountGenerator.cs
+++ DatabasePopulation/DatabasePopulation/AccountGenerator.cs
@@ -7,14 +7,18 @@ namespace DatabasePopulation
     class AccountGenerator
     {
         WordListManager words;
+        //If we don't get a WordListManager instance, we need to make one.
         public AccountGenerator()
         {
             words = new WordListManager();
         }
+        //If we do get a WordListManager instance, we're happy to save the resources. 
         public AccountGenerator(WordListManager words)
         {
             this.words = words;
         }
+        //This function generates a new account by collecting firstName, lastName, and password information
+        //from the WordListManager. It returns an instance of the class Account based on that constructor.
         public Account newAccount()
         {
             string firstName = words.getName();
@@ -22,6 +26,8 @@ namespace DatabasePopulation
             string password = words.getName();
             return new Account(firstName, lastName, password);
         }
+        //This function generates a list of account instances according to the number requested. 
+        //This is particularly useful in SQLInterface and CreatesGenerator.
         public List<Account> generateAccounts(int number)
         {
             List<Account> accounts = new List<Account>();
