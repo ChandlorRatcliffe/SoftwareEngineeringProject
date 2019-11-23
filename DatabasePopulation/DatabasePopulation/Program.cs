14359fcd0010738cd1243726a83afc347de96123
chandlor.ratcliffe <chandlor.ratcliffe@gmail.com>
2019-11-22 23:10:06 -0600

    Document Program.cs

--- DatabasePopulation/DatabasePopulation/Program.cs
+++ DatabasePopulation/DatabasePopulation/Program.cs
@@ -7,8 +7,11 @@ namespace DatabasePopulation
     {
         static void Main(string[] args)
         {
+            //Make an interface.
             SQLInterface sql = new SQLInterface();
+            //Clear the Database.
             sql.reset();
+            //Populate the Database.
             sql.generateCreateSets(125);
         }
     }
