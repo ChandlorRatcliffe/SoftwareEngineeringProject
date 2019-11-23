01b45638670d64aa06ab090a4a772990d23fe5b6
chandlor.ratcliffe <chandlor.ratcliffe@gmail.com>
2019-11-22 23:30:00 -0600

    Revert "Documented Project.cs"
    This reverts commit 95b5f9ff7da929a4bc9de0f07f24c50ad5409364.

--- DatabasePopulation/DatabasePopulation/Project.cs
+++ DatabasePopulation/DatabasePopulation/Project.cs
@@ -6,9 +6,6 @@ namespace DatabasePopulation
 {
     public class Project
     {
-        //We'll use a static id as a counter. Each new instance's myID will be defined by the current id,
-        //and then the id will be incremented again. 
-        //TODO: Replace this system with an alphanumeric id system with the same concept in place. 
         static int id = 0;
         int myId;
         DateTime deadline;
@@ -19,19 +16,16 @@ namespace DatabasePopulation
             deadline = DateTime.Today;
             description = "I don't know how this got here.";
         }
-        //A project minimally requires a deadline and a description. 
         public Project(DateTime deadline, string description)
         {
             myId = id++;
             this.deadline = deadline;
             this.description = description;
         }
-        //This generates the tuple required for the insert query string. 
         public string getAddTupleQuerryString()
         {
             return "(" +myId + ",\"" + deadline.ToString("yyyy-MM-dd HH:mm:ss") + "\",\"" + description+ "\")";
         }
-        //This provides the primary key for use in generating relation records. 
         public string getPK()
         {
             return "\"" + this.myId + "\"";
