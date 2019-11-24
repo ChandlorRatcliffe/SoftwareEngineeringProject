b719872d738b222646c7939274e4d8bae65c2654
idletravis <idle.travis@yahoo.com>
2019-11-24 14:23:09 -0600

    Added Willfredo's AddRemoveUser.

--- KarbonCopy/KarbonWebForms/Views/Projects/AddRemoveUser.aspx.cs
+++ KarbonCopy/KarbonWebForms/Views/Projects/AddRemoveUser.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace KarbonWebForms
+namespace KarbonWebForms.Views.Projects
 {
-    public partial class _Default : Page
+    public partial class AddRemoveUser : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
