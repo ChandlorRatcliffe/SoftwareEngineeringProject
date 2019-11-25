1685024585b3236e4f7296bb9d88bacf303ea14d
idletravis <idle.travis@yahoo.com>
2019-11-24 22:48:46 -0600

    Intention is only to keep the use of the Session variable in the codebehind. Will need to be updated to use the SQL Integration SQLFunctions coming in the merge.

--- KarbonCopy/KarbonWebForms/Views/Accounts/Login.aspx.cs
+++ KarbonCopy/KarbonWebForms/Views/Accounts/Login.aspx.cs
@@ -23,12 +23,17 @@ namespace CashewWebForms
             //Web.config contains the connection string so it doesn't have to be initialized every time. the below is how to access it
             var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["mySql"].ConnectionString);
             conn.Open();
-            var command = new MySqlCommand("Select Username From Account Where (Username = @user)", conn);
+            var command = new MySqlCommand("Select Username, Email, Password From Account Where (Username = @user AND Password = @pass);", conn);
             command.Parameters.Add(new MySqlParameter("user", MySqlDbType.VarChar) { Value = EnterUsername.Text });
+            command.Parameters.Add(new MySqlParameter("pass", MySqlDbType.VarChar) { Value = EnterPassword.Text });
             DataTable dt = new DataTable();
             dt.Load(command.ExecuteReader());
-            foreach (DataRow row in dt.Rows) {
-                Debug.WriteLine(row["Username"].ToString());
+            if (dt.Rows != null){ // the account with username/password exists and it matches
+                Session["Username"] = EnterUsername.Text;
+                Session["Email"] = dt.Rows[0][1]; // first row, second index because 0 = Username, 1 = Email, 2 = Password
+                Response.Redirect("~/Views/Projects/Projects");
+            } else {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alert", "Your Message", true);
             }
         }
     }
