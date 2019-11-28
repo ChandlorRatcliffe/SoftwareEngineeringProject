9e28cf3c5d75564eeb06e59a425a36323a335176
findhamza <hxs034@shsu.edu>
2019-11-27 19:52:18 -0600

    Revert "Merge branch 'WebFormsConversion' into frontend_hamza"
    This reverts commit d2366056713c86521f8a42a2d0ea00846c33b9f4, reversing
    changes made to cfadba34f6a3fdbe4ebde963a42b1d63cf68d3b1.

--- KarbonCopy/KarbonWebForms/Views/Accounts/Create.aspx.cs
+++ KarbonCopy/KarbonWebForms/Views/Accounts/Create.aspx.cs
@@ -12,11 +12,7 @@ namespace KarbonWebForms.Views.Accounts
 {
     public partial class Create : Page
     {
-        private readonly AccountSql accountSql = new AccountSql();
-        private readonly EmailTool emailTool = new EmailTool();
-        //make dynamic
-        private readonly string verfToken = "38749384";
-
+        //private readonly AccountSql accountSql = new AccountSql();
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -25,38 +21,25 @@ namespace KarbonWebForms.Views.Accounts
 
         protected async void CreateAccount(object sender, EventArgs e)
         {
-            //make dynamic
-            string URL = $"https://{Request.Url.Host}:{Request.Url.Port}/Views/Accounts/Verification";
-
-            if (Password.Text == ConfirmPassword.Text)
-            {
-                Account account = new Account
-                {
-                    Username = Username.Text,
-                    Email = Email.Text,
-                    FirstName = FirstName.Text,
-                    LastName = LastName.Text,
-                    Password = Password.Text,
-                    Skills = "false" //Not Verified
-                };
-                if (await emailTool.SendEmail(account, "Account Verification", "",
-                    $"Hello {account.FirstName}, <br> your karbon activation token is {verfToken} <br> {URL}"))
-                {
-                    accountSql.Insert(account);
-                    Debug.WriteLine("Verfication Token successfully sent and account added.");
-                    Response.Redirect("~/Views/Accounts/Login", false);
-                }
-                else
-                {
-                    Debug.WriteLine("Unable to send Email. Account not added.");
-                    Response.Redirect("~/Views/Accounts/Login", false);
-                }
-            }
-            else
-            {
-                //Validate Error Passwords don't match
-                Response.Redirect("~/Views/Accounts/Login", false);
-            }
+            //if (Password.Text == ConfirmPassword.Text)
+            //{
+            //    Account account = new Account
+            //    {
+            //        Username = Username.Text,
+            //        Email = Email.Text,
+            //        FirstName = FirstName.Text,
+            //        LastName = LastName.Text,
+            //        Password = Password.Text
+            //    };
+            //    accountSql.Insert(account);
+            //    //Redirect to Email Verfication Page NOT Implemented
+            //    Debug.WriteLine("Redirect to Email Page Not Implemented");
+            //    Response.Redirect("~/Views/Accounts/Login");
+            //}
+            //else
+            //{
+            //    //Validate Error Passwords don't match
+            //}
         }
 
     }
