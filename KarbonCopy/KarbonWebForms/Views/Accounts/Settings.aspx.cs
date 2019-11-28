60f0406f516c751b43f3908b91a8b43560b6be5c
ChandlorRatcliffe <47456805+ChandlorRatcliffe@users.noreply.github.com>
2019-11-27 20:43:56 -0600

    Revert "Frontend hamza"

--- KarbonCopy/KarbonWebForms/Views/Accounts/Settings.aspx.cs
+++ KarbonCopy/KarbonWebForms/Views/Accounts/Settings.aspx.cs
@@ -1,7 +1,5 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,29 +9,9 @@ namespace KarbonWebForms.Views.Accounts
 {
     public partial class Settings : System.Web.UI.Page
     {
-        protected void dark_Click(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
-            var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["mySql"].ConnectionString);
-            conn.Open();
-            string qRun = "UPDATE `account` SET `Theme`='dark' WHERE `Username`='" + Session["Username"].ToString() + "'";
-            var command = new MySqlCommand(qRun, conn);
-            command.ExecuteReader();
-            conn.Close();
-            Session["Theme"] = "dark";
-            Response.Redirect("~/Views/Accounts/Settings.aspx");
-        }
 
-        protected void light_Click(object sender, EventArgs e)
-        {
-            var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["mySql"].ConnectionString);
-            conn.Open();
-            string qRun = "UPDATE `account` SET `Theme`='light' WHERE `Username`='" + Session["Username"].ToString() + "'";
-            var command = new MySqlCommand(qRun, conn);
-            command.ExecuteReader();
-            conn.Close();
-            Session["Theme"] = "light";
-            Response.Redirect("~/Views/Accounts/Settings.aspx");
         }
-
     }
 }
