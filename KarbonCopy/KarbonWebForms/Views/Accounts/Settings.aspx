a42acae197da716e76351f131f5e01460d93e317
Timothy Manno <38054853+tmanno1987@users.noreply.github.com>
2019-11-29 01:31:50 -0600

    Settings Update
    Updated the settings file with regards to changing username, password and email.

--- KarbonCopy/KarbonWebForms/Views/Accounts/Settings.aspx
+++ KarbonCopy/KarbonWebForms/Views/Accounts/Settings.aspx
@@ -14,9 +14,9 @@
             </div>
             <div class="input-group mb-4">
                 <br />
-                <input type="text" class="form-control" placeholder="New Username" aria-label="New Username" aria-describedby="button-addon1">
+                <asp:TextBox runat="server" ID="changeUser" class="form-control" Text="New Username" aria-label="New Username" aria-describedby="button-addon1"></asp:TextBox>
                 <div class="input-group-append">
-                    <button class="btn btn-dark" type="button" id="button-addon1">Submit</button>
+                    <button runat="server" class="btn btn-dark" type="button" id="button-addon1" onclick="changeUsername">Submit</button>
                 </div>
             </div>
             <br />
@@ -25,9 +25,9 @@
                 Email
             </div>
             <div class="input-group mb-4">
-                <input type="text" class="form-control" placeholder="New Email" aria-label="New Email" aria-describedby="button-addon2">
+                <asp:TextBox ID="changeEmail" runat="server" class="form-control" Text="New Email" aria-label="New Email" aria-describedby="button-addon2"></asp:TextBox>
                 <div class="input-group-append">
-                    <button class="btn btn-dark" type="button" id="button-addon2">Submit</button>
+                    <button runat="server" class="btn btn-dark" type="button" id="button-addon2" onclick="changeEmail">Submit</button>
                 </div>
             </div>
             <br />
@@ -36,12 +36,12 @@
                 Password
             </div>
             <div class="input-group mb-4">
-                <input type="text" class="form-control" placeholder="New Password" aria-label="New Password" aria-describedby="button-addon3">
+                <asp:TextBox ID="changePassword" runat="server" class="form-control" Text="New Password" aria-label="New Password" aria-describedby="button-addon3"></asp:TextBox>
             </div>
             <div class="input-group mb-4">
-                <input type="text" class="form-control" placeholder="Confirm New Password" aria-label="Confirm New Password" aria-describedby="button-addon2">
+                <asp:TextBox ID="confirmPassword" class="form-control" Text="Confirm New Password" aria-label="Confirm New Password" aria-describedby="button-addon2"></asp:TextBox>
                 <div class="input-group-append">
-                    <button class="btn btn-warning" type="button" id="button-addon3">Submit</button>
+                    <button runat="server" class="btn btn-warning" type="button" id="button-addon3" onclick="changePassword">Submit</button>
                 </div>
             </div>
             <!-- Change Theme Section-->
--- KarbonCopy/KarbonWebForms/Views/Accounts/Settings.aspx.cs
+++ KarbonCopy/KarbonWebForms/Views/Accounts/Settings.aspx.cs
@@ -15,25 +15,60 @@ namespace KarbonWebForms.Views.Accounts
         {
             var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["mySql"].ConnectionString);
             conn.Open();
-            string qRun = "UPDATE `account` SET `Theme`='dark' WHERE `Username`='" + Session["Username"].ToString() + "'";
+            string qRun = "UPDATE `account` SET `Theme`='dark' WHERE `Username`='" + Session["Username"].ToString() + "';";
             var command = new MySqlCommand(qRun, conn);
             command.ExecuteReader();
             conn.Close();
             Session["Theme"] = "dark";
-            Response.Redirect("~/Views/Accounts/Settings.aspx");
+            Response.Redirect("/Views/Accounts/Settings.aspx");
         }
 
         protected void light_Click(object sender, EventArgs e)
         {
             var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["mySql"].ConnectionString);
             conn.Open();
-            string qRun = "UPDATE `account` SET `Theme`='light' WHERE `Username`='" + Session["Username"].ToString() + "'";
+            string qRun = "UPDATE `account` SET `Theme`='light' WHERE `Username`='" + Session["Username"].ToString() + "';";
             var command = new MySqlCommand(qRun, conn);
             command.ExecuteReader();
             conn.Close();
             Session["Theme"] = "light";
-            Response.Redirect("~/Views/Accounts/Settings.aspx");
+            Response.Redirect("/Views/Accounts/Settings.aspx");
         }
-
+		
+		protected void changeUsername(object sender, EventArgs e)
+		{
+			var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["mySql"].ConnectionString);
+            conn.Open();
+            string qRun = "UPDATE `account` SET `Username`=@newUser WHERE `Username`='" + Session["Username"].ToString() + "';";
+            var command = new MySqlCommand(qRun, conn);
+			command.Parameters.Add(new MySqlParameter("newUser", MySqlDbType.VarChar) { Value = changeUser.Text });
+            command.ExecuteReader();
+            conn.Close();
+		}
+		
+		protected void changeEmail(object sender, EventArgs e)
+		{
+			var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["mySql"].ConnectionString);
+            conn.Open();
+            string qRun = "UPDATE `account` SET `Email`=@email WHERE `Username`='" + Session["Username"].ToString() + "';";
+            var command = new MySqlCommand(qRun, conn);
+			command.Parameters.Add(new MySqlParameter("email", MySqlDbType.VarChar) { Value = changeEmail.Text });
+            command.ExecuteReader();
+            conn.Close();
+		}
+		
+		protected void changePassword(object sender, EventArgs e)
+		{
+			if (confirmPassword.Text == changePassword.Text)
+			{
+				var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["mySql"].ConnectionString);
+				conn.Open();
+				string qRun = "UPDATE `account` SET `Password`=@pw WHERE `Username`='" + Session["Username"].ToString() + "';";
+				var command = new MySqlCommand(qRun, conn);
+				command.Parameters.Add(new MySqlParameter("pw", MySqlDbType.VarChar) { Value = changePassword.Text });
+				command.ExecuteReader();
+				conn.Close();
+			}
+		}
     }
 }
